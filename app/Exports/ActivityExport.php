110c50fc0812e1f7563214c8923c2a82327666c1
phyzerbert <xian1017@outlook.com>
2019-11-25 13:51:44 +0800

    adfasf

--- app/Exports/ActivityExport.php
+++ app/Exports/ActivityExport.php
@@ -3,15 +3,35 @@
 namespace App\Exports;
 
 use App\Activity;
-use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\FromArray;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ActivityExport implements FromCollection
+class ActivityExport implements FromArray, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
-    public function collection()
+    public function array(): array
     {
-        return Activity::all();
+        $data =  Activity::distinct('phone_number')->get();
+        $activities = array();
+        $i = 0;
+        foreach ($data as $item) {
+            $activities[$i] = array();
+            $activities[$i]['no'] = $i+1;
+            $activities[$i]['phone_number'] = $item->phone_number;
+            $activities[$i]['date_time'] = $item->creatd_at;
+            $i++;
+        }
+        return $activities;
+    }
+    
+    public function headings(): array
+    {
+        return [
+            'No',
+            'Phone Number',
+            'Date & Time',
+        ];
     }
 }
